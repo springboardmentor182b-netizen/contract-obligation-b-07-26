9ea96b2db80b9f49794c644f3b9901af14d2245d
Komma Anushka <251626100+AnushkaReddyNITRR@users.noreply.github.com>
2026-07-09 11:43:36 +0530

    Delete App.jsx

--- App.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ObligationTrackerPage from "./features/obligationTracker/ObligationTrackerPage";
-
-export default function App() {
-  return <ObligationTrackerPage />;
-}
