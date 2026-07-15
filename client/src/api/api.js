7e92e093396c306d06763e304c5772c35d9a8961
dedipya03 <162884718+dedipya03@users.noreply.github.com>
2026-07-15 16:29:18 +0530

    Obligation-Tracker-Module-divi-dedipya

---
+++ client/src/api/api.js
@@ -0,0 +1,44 @@
+const BASE_URL = "http://127.0.0.1:8000";
+
+export default BASE_URL;
+
+// Get all obligations
+export async function getObligations() {
+    const response = await fetch(`${BASE_URL}/obligations`);
+    return response.json();
+}
+
+// Create
+export async function createObligation(data) {
+    const response = await fetch(`${BASE_URL}/obligations`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    return response.json();
+}
+
+// Update
+export async function updateObligation(id, data) {
+    const response = await fetch(`${BASE_URL}/obligations/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    return response.json();
+}
+
+// Delete
+export async function deleteObligation(id) {
+    const response = await fetch(`${BASE_URL}/obligations/${id}`, {
+        method: "DELETE",
+    });
+
+    return response.json();
+}
