afaebc2d8489f6c0abcd78f25b12595e674da3e5
swathi <swathibolleni26@gmail.com>
2026-07-21 00:20:49 +0530

    Added renewal management backend integration

--- server/main.py
+++ server/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
 
-app = FastAPI()
+from src.database.core import Base, engine
+
+from src.renewals.models import Renewal
+from src.renewals.controller import router as renewal_router
 
 
-@app.get('/')
-def hello_world():
-    return{'HEllo':'World'}
+app = FastAPI(title="ContractIQ Backend")
+
+
+Base.metadata.create_all(bind=engine)
+
+
+app.include_router(renewal_router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "ContractIQ Backend Running Successfully"
+    }
