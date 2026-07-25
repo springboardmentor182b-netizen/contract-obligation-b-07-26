906408930bbc2df0101d78909c809c819a262670
Varun Boga <varunboga2005@gmail.com>
2026-07-25 12:14:13 +0530

    Update main.py

--- server/app/main.py
+++ server/app/main.py
@@ -7,6 +7,9 @@ from app.contracts.router import router as contracts_router
 from app.reports.router import router as reports_router
 from app.users.router import router as users_router
 from app.settings.router import router as settings_router
+from app.auth.router import router as auth_router
+
+app.include_router(auth_router)
 
 app.include_router(reports_router)
 app.include_router(users_router)
