8ba4c909c8e570fb0548ba7dadaa4574afc624a2
gdoddala <gdoddala@student.gitam.edu>
2026-07-28 11:00:38 +0530

    Move users API base URL to environment variable

--- Client/src/services/usersApi.js
+++ Client/src/services/usersApi.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
