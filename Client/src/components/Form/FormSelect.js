d2bf17cfead065fc859f03e09782761fb3b3bdef
ENUGURTHI HARSHITHA <harshitha@gmail.com>
2026-08-03 21:36:48 +0530

    Fix empty form components

---
+++ Client/src/components/Form/FormSelect.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function FormSelect({ label, value, onChange, options }) {
+  return (
+    <div className="mb-3">
+      <label className="form-label fw-semibold">{label}</label>
+      <select className="form-select" value={value} onChange={onChange}>
+        {options.map((item) => (
+          <option key={item} value={item}>{item}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default FormSelect;
