f9b10804ac6ae8beafca18c5e672f85a99761019
dedipya03 <162884718+dedipya03@users.noreply.github.com>
2026-08-03 19:42:36 +0530

    User-Management-Module-divi-dedipya

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,3 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-
-import App from './App'
-import './assets/global.css'
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -36,6 +20,3 @@ root.render(
     </React.StrictMode>
 
 );
-createRoot(document.getElementById('root')).render(
-  React.createElement(React.StrictMode, null, React.createElement(App)),
-)
