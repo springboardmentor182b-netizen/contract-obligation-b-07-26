f225537a35cb031ccf92beb87de64c1d946bc4fc
dedipya03 <162884718+dedipya03@users.noreply.github.com>
2026-08-03 13:26:45 +0530

    User-Management-Module-divi-dedipya

--- server/src/main.py
+++ server/src/main.py
@@ -465,3 +465,43 @@ def home():
     return {
         "message": "Compliance Monitoring API Running Successfully"
     }
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.database.database import Base
+from app.database.database import engine
+
+from app.routers import auth
+from app.routers import users
+
+
+# Import models so SQLAlchemy creates the tables
+from app.models.role import Role
+from app.models.user import User
+from app.routers import roles
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="User Management API",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(roles.router)
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "User Management API Running Successfully"
+    }
