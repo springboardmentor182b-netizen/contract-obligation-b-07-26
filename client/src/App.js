6d9dd1030dec404cd68ff6bfb9ae2c4655e1759c
pragnasree82-ship-it <pragnasree82@gmail.com>
2026-08-06 16:58:24 +0530

    Resolve merge conflicts and update App.js routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,44 @@
-import "./App.css";
+import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
-import { BrowserRouter } from "react-router-dom";
+import './App.css'
+import Settings from './pages/Settings/Settings'
+import { UserProvider } from './context/UserContext'
+import Layout from './components/Layout/Layout'
 
-import AppRoutes from "./routes/AppRoutes";
-
-function App() {
-
-    return (
-
-        <BrowserRouter>
-
-            <AppRoutes />
-
-        </BrowserRouter>
-
-    );
-
+export default function App() {
+  return React.createElement(
+    UserProvider,
+    null,
+    React.createElement(
+      BrowserRouter,
+      null,
+      React.createElement(
+        Routes,
+        null,
+        React.createElement(Route, {
+          path: '/',
+          element: React.createElement(Navigate, {
+            to: '/settings',
+            replace: true,
+          }),
+        }),
+        React.createElement(Route, {
+          path: '/settings',
+          element: React.createElement(
+            Layout,
+            null,
+            React.createElement(Settings),
+          ),
+        }),
+        React.createElement(Route, {
+          path: '*',
+          element: React.createElement(Navigate, {
+            to: '/settings',
+            replace: true,
+          }),
+        }),
+      ),
+    ),
+  )
 }
-
-export default App;
